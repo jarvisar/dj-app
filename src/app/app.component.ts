97622fcbc10c8ac11c7a6c79e211a2de2924b8cd
jarvisar <jarvisar@mail.uc.edu>
2023-02-19 02:23:03 -0500

    Added websocket support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebSocketService } from './web-socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dj-app';
-}
+
+  
+  constructor(private websocketService: WebSocketService) {}
+
+  ngOnInit() { }
+
+  getTrackData(event: Event) {
+    console.log("hi");
+    this.websocketService.createQueue().subscribe(
+      (data: any) => {
+        console.log('Track Data:', data);
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+ }
+
---
+++ src/app/web-socket.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
+import { Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+  private socket!: Socket;
+
+  constructor() { 
+    this.socket = io('http://localhost:5000');
+  }
+
+  createQueue(): Observable<any> {
+
+    return new Observable(observer => {
+      this.socket.emit('create');
+
+      this.socket.on('queue_created', (data: any) => {
+        observer.next(data);
+      });
+
+    });
+  }
+
+  joinQueue(code: string) {
+    this.socket.emit('join', { code });
+  }
+
+  addSong(code: string, name: string, artist: string, trackId: string) {
+    this.socket.emit('add_song', { code, name, artist, trackId });
+  }
+
+  deleteSong(code: string, songId: string) {
+    this.socket.emit('delete_song', { code, songId });
+  }
+
+  deleteQueue() {
+    this.socket.emit('delete_queue');
+  }
+
+  onQueueCreated(): Observable<any> {
+    return new Observable((observer) => {
+      this.socket.on('queue_created', (data: any) => {
+        observer.next(data);
+      });
+    });
+  }
+  
+
+  onQueueJoined() {
+    return new Observable((observer) => {
+      this.socket.on('queue_joined', (data: any) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  onSongAdded() {
+    return new Observable((observer) => {
+      this.socket.on('song_added', (data: any) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  onSongDeleted() {
+    return new Observable((observer) => {
+      this.socket.on('song_deleted', (data: any) => {
+        observer.next(data);
+      });
+    });
+  }
+
+  onInvalidCode() {
+    return new Observable((observer) => {
+      this.socket.on('invalid_code', () => {
+        observer.next();
+      });
+    });
+  }
+
+  onInvalidSong() {
+    return new Observable((observer) => {
+      this.socket.on('invalid_song', () => {
+        observer.next();
+      });
+    });
+  }
+
+  onInvalidDelete() {
+    return new Observable((observer) => {
+      this.socket.on('invalid_delete', () => {
+        observer.next();
+      });
+    });
+  }
+}
