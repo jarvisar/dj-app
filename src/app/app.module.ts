e3dbc9d6ded7dacbfd882d6e78b87686b4055484
jarvisar <jarvisar@mail.uc.edu>
2023-02-20 23:47:22 -0500

    Added requests to table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { io } from 'socket.io-client';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule  } from '@angular/common/http';
+import { LoadingInterceptor } from './loading-interceptor.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,10 @@ import { HttpClientModule  } from '@angular/common/http';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatProgressSpinnerModule
   ],
-  providers: [HttpClientModule],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
