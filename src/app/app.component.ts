5778b81c1ca148b4f28db0e3244f072c805e7fd4
jarvisar <jarvisar@mail.uc.edu>
2023-03-03 15:31:12 -0500

    Fixed adding songs sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,9 +43,11 @@ export class AppComponent implements OnInit {
             this.errorMessage = ''; 
           }, 3000); 
         } else {
+          console.log('Code:', data);
+          this.errorMessage = '';
+          this.inputCode = data.code;
+          this.joinQueue();
         }
-        console.log('Code:', data);
-        this.errorMessage = '';
       },
       (error) => {
         console.error('Error:', error);
@@ -63,7 +65,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  joinQueue(event: Event) {
+  joinQueue(event?: Event) {
     console.log("join");
     this.websocketService.joinQueue(this.inputCode).subscribe(
       (data: any) => {
@@ -126,6 +128,7 @@ export class AppComponent implements OnInit {
     this.showQueueSongTable = false;
     this.numVotes = 0;
     this.requestedSongs = [];
+    this.maxVotes = false;
   }
 
 requestedSongs: any = [];
@@ -145,21 +148,13 @@ addSong(event: Event, track: any) {
         break;
       }
     }
-    if (isTrackInQueue) { // already in queueSongs
+    if (isTrackInQueue) { // already in queueSongs, so just update count
       this.websocketService.addSong(this.setCode, track.name, track.artists[0].name, track.id).subscribe(
         ({ songs }: any) => {
           this.requestedSongs.push(track.id);
           this.queueSongs[index].count += 1; // Only update count client-side
           this.numVotes++;
-          if(this.queueSessionId == this.websocketService.sessionID){
-            if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
-              this.maxVotesReached();
-            }
-          } else {
-            if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
-              this.maxVotesReached();
-            }
-          }
+          this.queueSongs = this.queueSongs.sort((a: any, b: any) => b.count - a.count);
         },
         (error) => {
           console.error('Error:', error);
@@ -169,7 +164,7 @@ addSong(event: Event, track: any) {
           }, 3000); // remove class after 5 seconds
         }
       );
-    } else { // song not in queueSongs, need to add
+    } else { // song not in queueSongs, need to add and update queueSongs with new song
       this.websocketService.addSong(this.setCode, track.name, track.artists[0].name, track.id).subscribe(
         ({ songs }: any) => {
           this.requestedSongs.push(track.id);
@@ -178,17 +173,8 @@ addSong(event: Event, track: any) {
             trackData[0].count = 1; // song not in queue so must be first request
             this.queueSongs.push(trackData[0]);
           });
-          this.queueSongs = this.queueSongs.sort((a: any, b: any) => b.count - a.count);
           this.numVotes++;
-          if(this.queueSessionId == this.websocketService.sessionID){
-            if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
-              this.maxVotesReached();
-            }
-          } else {
-            if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
-              this.maxVotesReached();
-            }
-          }
+          this.queueSongs = this.queueSongs.sort((a: any, b: any) => b.count - a.count);
         },
         (error) => {
           console.error('Error:', error);
@@ -199,7 +185,15 @@ addSong(event: Event, track: any) {
         }
       );
     }
-
+    if(this.queueSessionId == this.websocketService.sessionID){
+      if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
+        this.maxVotesReached();
+      }
+    } else {
+      if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
+        this.maxVotesReached();
+      }
+    }
   } else {
     console.log("Join a queue before adding a song");
   }
