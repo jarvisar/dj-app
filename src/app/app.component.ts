86fe58a80aa5fd98f7e8643542f56c3182b887d9
jarvisar <jarvisar@mail.uc.edu>
2023-03-03 15:23:41 -0500

    Dramatically increased performance adding songs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -131,47 +131,75 @@ export class AppComponent implements OnInit {
 requestedSongs: any = [];
 addSong(event: Event, track: any) {
   console.log("add song");
+  let isTrackInQueue = false;
+  let index = 0;
   if (this.setCode) {
     if (this.requestedSongs.includes(track.id)) {
       console.log("Song has already been requested");
       return;
     }
-    this.websocketService.addSong(this.setCode, track.name, track.artists[0].name, track.id).subscribe(
-      ({ songs }: any) => {
-        this.requestedSongs.push(track.id);
-        const idList = songs.map((song: any) => song.id);
-        this.websocketService.getTrackData(idList).subscribe((trackData: any) => {
-          trackData.forEach((song: any) => {
-            const { count = 0 } = songs.find((s: any) => s.id === song.id) || {};
-            song.count = count;
-          });
-          this.queueSongs = trackData.sort((a: any, b: any) => b.count - a.count);
-          if (this.queueSongs.length !== 0) {
-            this.showQueueSongTable = true;
-          }
-        });
-        this.numVotes++;
-        console.log(this.numVotes);
-        if(this.queueSessionId == this.websocketService.sessionID){
-          console.log("test")
-          if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
-            this.maxVotesReached();
-          }
-        } else {
-          if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
-            this.maxVotesReached();
-          }
-        }
-        console.log(this.requestedSongs);
-      },
-      (error) => {
-        console.error('Error:', error);
-        this.errorMessage = 'Error Adding Song'
-        setTimeout(() => {
-          this.errorMessage = ''; // remove class after animation ends
-        }, 3000); // remove class after 5 seconds
+    for (let i = 0; i < this.queueSongs.length; i++) {
+      if (this.queueSongs[i].id === track.id) { // Check if song is already in queueSongs
+        isTrackInQueue = true;
+        index = i;
+        break;
       }
-    );
+    }
+    if (isTrackInQueue) { // already in queueSongs
+      this.websocketService.addSong(this.setCode, track.name, track.artists[0].name, track.id).subscribe(
+        ({ songs }: any) => {
+          this.requestedSongs.push(track.id);
+          this.queueSongs[index].count += 1; // Only update count client-side
+          this.numVotes++;
+          if(this.queueSessionId == this.websocketService.sessionID){
+            if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
+              this.maxVotesReached();
+            }
+          } else {
+            if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
+              this.maxVotesReached();
+            }
+          }
+        },
+        (error) => {
+          console.error('Error:', error);
+          this.errorMessage = 'Error Adding Song'
+          setTimeout(() => {
+            this.errorMessage = ''; // remove class after animation ends
+          }, 3000); // remove class after 5 seconds
+        }
+      );
+    } else { // song not in queueSongs, need to add
+      this.websocketService.addSong(this.setCode, track.name, track.artists[0].name, track.id).subscribe(
+        ({ songs }: any) => {
+          this.requestedSongs.push(track.id);
+          this.websocketService.getTrackData([track.id]).subscribe((trackData: any) => {
+            console.log(trackData)
+            trackData[0].count = 1; // song not in queue so must be first request
+            this.queueSongs.push(trackData[0]);
+          });
+          this.queueSongs = this.queueSongs.sort((a: any, b: any) => b.count - a.count);
+          this.numVotes++;
+          if(this.queueSessionId == this.websocketService.sessionID){
+            if(this.numVotes >= (maxVotes * 2)){ // Queue creators can add 10 starter songs
+              this.maxVotesReached();
+            }
+          } else {
+            if(this.numVotes >= (maxVotes)){ // Other users can add 5 songs to a queue
+              this.maxVotesReached();
+            }
+          }
+        },
+        (error) => {
+          console.error('Error:', error);
+          this.errorMessage = 'Error Adding Song'
+          setTimeout(() => {
+            this.errorMessage = ''; // remove class after animation ends
+          }, 3000); // remove class after 5 seconds
+        }
+      );
+    }
+
   } else {
     console.log("Join a queue before adding a song");
   }
