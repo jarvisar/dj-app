fc685e22122aa8f5253e5735909bc23018d6bd56
jarvisar <jarvisar@mail.uc.edu>
2023-03-04 00:14:27 -0500

    Added spotify search route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CreateQueueComponent } from './create-queue/create-queue.component';
 import { HomeComponent } from './home/home.component';
 import { JoinQueueComponent } from './join-queue/join-queue.component';
 import { QueueViewComponent } from './queue-view/queue-view.component';
+import { SpotifySearchComponent } from './spotify-search/spotify-search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'join', component: JoinQueueComponent },
   { path: 'create', component: CreateQueueComponent },
-  { path: 'queue', component: QueueViewComponent }
+  { path: 'queue', component: QueueViewComponent },
+  { path: 'search', component: SpotifySearchComponent }
 ];
 
 @NgModule({
